24994d1821b774df3709d507a84d02a2daeaed24
AAKAAASSHHH24 <akashrksht@gmail.com>
2023-02-12 23:42:27 +0530

    testing ingestion

---
+++ tests/integration/dataingestiontest.py
@@ -0,0 +1,29 @@
+import os
+import logging
+from pathlib import Path
+from cifar100classifier.entity import DataIngestionConfig
+from src.cifar100classifier.components.data_ingestion import (
+    DataIngestion,
+    DataIngestionConfig,
+)
+import pytest
+
+logger = logging.getLogger(__name__)
+
+
+class Test_DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def test_download_file(self):
+        self.data_ingestion.download_file()
+        assert os.path.exists(self.local_data_file)
+        assert os.path.getsize(self.local_data_file) > 0
+
+    def test_unzip_and_clean(self):
+        with pytest.raises(Exception):
+            self.data_ingestion.unzip_and_clean()
+        extracted_files = [
+            f for f in os.listdir(self.unzip_dir) if not f.endswith(".txt~")
+        ]
+        assert len(extracted_files) == 0
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -6,20 +6,39 @@ from ensure.main import EnsureError
 
 
 class Test_read_yaml:
-    yaml_files = [
+    yaml_files = ["tests/data/demo.yaml",
         "configs/config.yaml"
     ]
 
     def test_read_yaml_empty(self):
+        
+        """This test method tests if the read_yaml function raises a ValueError 
+        when reading an empty YAML file. It uses the pytest.raises 
+        context manager to catch the expected exception."""
+        
         with pytest.raises(ValueError):
             read_yaml(Path(self.yaml_files[0]))
 
     def test_read_yaml_return_type(self):
+        
+        """This test method tests if the read_yaml function returns a ConfigBox object. 
+        It calls the read_yaml function on the second file in the yaml_files list 
+        and checks if the return value is of type ConfigBox using the isinstance function."""
+        
         respone = read_yaml(Path(self.yaml_files[-1]))
         assert isinstance(respone, ConfigBox)
 
     @pytest.mark.parametrize("path_to_yaml", yaml_files)   # passing multiple inputs to test
     def test_read_yaml_bad_type(self, path_to_yaml):
+        
+        """This test method tests if the read_yaml function raises an EnsureError 
+        when reading a YAML file with incorrect data types. 
+        The method is decorated with pytest.mark.parametrize which allows 
+        multiple inputs to be passed to the test. The path_to_yaml argument 
+        is passed in from the yaml_files list, and the test checks if the read_yaml 
+        function raises an EnsureError when reading each file. It uses the pytest.raises context manager 
+        to catch the expected exception.
+        """
         with pytest.raises(EnsureError):
             read_yaml(path_to_yaml)
 
